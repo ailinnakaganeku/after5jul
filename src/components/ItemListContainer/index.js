b9c64b0d49664aa906f87741fe6508509039e815
ailin.nakaganeku <ailin.nakaganeku@zenvia.com>
2022-07-05 17:56:43 -0300

    comments

--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -8,12 +8,15 @@ const ItemListContainer = ({ greeting }) => {
   const [loading, setLoading] = useState(true);
   console.log("products: ", products);
 
-  /* WITH THEN CATCH AND FINALLY 
+  /* CHOOSE ONE OF THEM: PROMISES OR ASYNC AWAIT */
+
+  /* PROMISES WITH THEN CATCH AND FINALLY 
     THEN IS GOING TO BE CALLED WHEN OUR PROMISE IS RESOLVED
     CATCH IS USED TO HANDLE ERRORS
-    FINALLY IS USED TO HANDLE THE END OF THE PROMISE */
+    FINALLY IS USED TO HANDLE THE END OF THE PROMISE REGARDLESS OF THE STATE OF THE PROMISE. */
 
   useEffect(() => {
+    console.log("WITH THEN CATCH AND FINALLY");
     getData
       .then((data) => {
         setProducts(data);
@@ -26,26 +29,26 @@ const ItemListContainer = ({ greeting }) => {
       });
   }, []);
 
-  /* WITH ASYNC/AWAIT TRY CATCH FINALLY */
-
-  /* ASYNCHRONOUS CODE IS USED TO EXECUTE STH (ASYNC)
+  /* WITH ASYNC/AWAIT TRY CATCH FINALLY
+    ASYNC/AWAIT IS A SYNTACTIC SUGAR FOR PROMISES.
+    ASYNCHRONOUS CODE IS USED TO EXECUTE STH (ASYNC)
     IN THE BACKGROUND SO THAT IT DOESN'T BLOCK THE EXECUTION OF THE REST OF THE CODE.
     IT IS GOING TO EXECUTE THAT ASYNC CODE AND THE REST OF THE CODE (NOT ASYNC CODE) AT THE SAME TIME.
     SO YOU MAY HAVE MULTIPLE DIFFERENT THREADS RUNNING YOUR CODE.
    */
 
-  const getProducts = async () => {
-    try {
-      const response = await getData;
-      setProducts(response);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   useEffect(() => {
+    console.log("WITH ASYNC/AWAIT TRY CATCH FINALLY");
+    const getProducts = async () => {
+      try {
+        const response = await getData;
+        setProducts(response);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
     getProducts();
   }, []);
 
